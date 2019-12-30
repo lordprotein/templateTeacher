b88211656076d22aed20399ecdf19151b7602e41
lordprotein <core.safecrlf>
2019-12-30 17:35:55 +0500

    add connect

--- app/src/component/menu/Menu.js
+++ app/src/component/menu/Menu.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MenuItem from '../menuItem/MenuItem';
+import dbService from '../dbService/dbService';
 
-const Menu = ({ elems }) => {
 
-    return (
-        <nav className="menu menu--line">
-            {
-                renderElems(elems)
-            }
-        </nav>
-    );
+export default class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuList: [],
+        }
+    }
+
+    componentDidMount() {
+        const { position } = this.props;
+        const service = new dbService();
+        service.getMenuPositionList(position)
+            .then(result => {
+                this.setState(() => { return { menuList: result } })
+            });
+    }
+
+    render() {
+        const { menuList } = this.state;
+        const { position } = this.props;
+
+        let stylePos = 'menu ';
+        if (position === 'top') stylePos += 'menu--line';
+
+        return (
+            <nav className={stylePos}>
+                {
+                    renderElems(menuList)
+                }
+            </nav>
+        );
+    }
+
 }
 
 const renderElems = (elems) => {
+    console.log(elems);
     return elems.map((item, key) => {
         return (
             <MenuItem
                 title={item.title}
-                link={item.link}
+                link='#'
                 key={key}
             />
         )
     });
 }
-
-export default Menu;
