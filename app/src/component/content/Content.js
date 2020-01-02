d5d99d89a2dd174649e388df8634fe71831fc98b
lordprotein <core.safecrlf>
2020-01-02 21:11:30 +0500

    add api service and logic

--- app/src/component/content/Content.js
+++ app/src/component/content/Content.js
@@ -1,23 +1,75 @@
 import React, { Component } from 'react';
+import ContentItem from '../contentItem/ContentItem';
+import { Switch, Route } from 'react-router-dom';
+import dbService from '../dbService/dbService';
+import toNormalizeLink from '../normalizeLink/normalizeLink';
+
+
 
 export default class Content extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+    state = {
+        menus: []
+    }
 
-        }
+    componentDidMount = async () => {
+        const db = new dbService();
+
+        const menuList = await db.getMenuList();
+
+        const menus = menuList.map(async ({ ID, title }) => {
+            const postList = await db.getContent(ID)
+            return {
+                title: title,
+                link: `/${toNormalizeLink(title)}`,
+                posts: postList
+            }
+        })
+
+        const resMenu = await Promise.all(menus);
+
+        this.setState(() => {
+            return { menus: resMenu }
+        })
+    }
+
+    generatePosts = (menuItem = []) => {
+        return menuItem.map(({ title, content }, key) => {
+            return (
+                <ContentItem
+                    title={title}
+                    content={content}
+                    key={key}
+                />
+            )
+        })
+    }
+
+    get createRoute() {
+        const { menus } = this.state;
+
+        if (!menus.length) return false;
+
+        return menus.map(({ link, posts }, key) => {
+
+            return <Route
+                path={link}
+                render={() => this.generatePosts(posts)}
+                key={key} />
+        });
     }
 
     render() {
         return (
-            <>
-                <h2 class="conten__title">Title</h2>
-                <p class="content__text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad magnam a iure pariatur? Illum, doloremque odio
-                    eaque corporis ipsum voluptatibus assumenda voluptatem, dignissimos quaerat dicta est nobis autem ratione
-                    itaque!
-		        </p>
-            </>
+            <section className="content">
+                <Switch>
+                    <Route exact path='/' render={() => <Te />} />
+                    {this.createRoute}
+                </Switch>
+            </section>
         );
     }
+}
+
+const Te = () => {
+    return <div>Hello!</div>;
 }
