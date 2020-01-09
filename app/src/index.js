41c8d56f94742611d871156e12c5e5a915f40620
lordprotein <core.safecrlf>
2020-01-09 15:54:46 +0500

    connect redux with react

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './component/app/App';
 import { BrowserRouter } from 'react-router-dom';
+import reducer from './redux/reducer';
+
 
 const AppRender = () => {
+    const store = createStore(reducer);
+    
     return (
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     );
 
 }
