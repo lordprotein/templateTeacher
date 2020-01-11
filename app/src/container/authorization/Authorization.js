b277be36e10409b3cbba372c469feac260c025d2
lordprotein <core.safecrlf>
2020-01-11 15:41:54 +0500

    component Authorization

---
+++ app/src/container/authorization/Authorization.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../redux/actions';
+import selectors from '../../redux/reducer';
+import dbService from '../../dbService/dbService';
+
+
+
+class Authorization extends Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const data = {
+            login: document.getElementsByName('auth-login')[0].value,
+            password: document.getElementsByName('auth-password')[0].value,
+        }
+        
+        const db = new dbService();
+        db.postLogin(data)
+            .then(({access}) => {
+                if (access) this.props.setLogIn(data);
+            })
+        // console.log(log, pass)
+    }
+
+    render() {
+
+
+        return (
+            <form>
+                <label>
+                    Логин
+                    <input type="text" name="auth-login" />
+                </label>
+                <label>
+                    Пароль
+                    <input type="text" name="auth-password" />
+                </label>
+                <input type="submit" value="Submit" onClick={this.handleSubmit} />
+            </form >
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    console.log(state);
+}
+
+const mapDispatchToProps = dispatch => {
+    const { setLogIn } = bindActionCreators(actions, dispatch);
+    return { setLogIn };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Authorization);
