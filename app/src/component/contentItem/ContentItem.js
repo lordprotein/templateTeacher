2867ecda316eb053f9e00d57f7d4b11693b8c732
lordprotein <core.safecrlf>
2020-01-12 16:15:25 +0500

    add function delete post in components

--- app/src/component/contentItem/ContentItem.js
+++ app/src/component/contentItem/ContentItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const ContentItem = ({ title, content }) => {
+const ContentItem = ({ ID, title, content, login, deletePost }) => {
     return (
         <>
             <h2 className="conten__title">{title}</h2>
             <p className="content__text">
                 {content}
             </p>
+            {login && (
+                <>
+                    <button>Редактироватьй</button>
+                    <button onClick={() => deletePost(ID)}>Удалить</button>
+                </>
+            )}
         </>
     );
 }
