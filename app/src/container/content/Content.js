b04953bdf3cedafff2fca81ee3f20012bd50a484
lordprotein <core.safecrlf>
2020-01-12 16:18:58 +0500

    add functions delete and add posts

--- app/src/container/content/Content.js
+++ app/src/container/content/Content.js
@@ -4,38 +4,76 @@ import { connect } from 'react-redux';
 import { selectors } from '../../redux/reducer'
 import ContentItem from '../../component/contentItem/ContentItem';
 import Authorization from '../authorization/Authorization';
-
+import FormAddPost from '../formAddPost/FormAddPost';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../redux/actions';
+import dbService from '../../dbService/dbService';
 
 
 class Content extends Component {
+    constructor(props) {
+        super(props);
+        this.db = new dbService();
+    }
 
-    generatePosts = (menuItem = []) => {
-        return menuItem.map(({ title, content }, key) => {
-            return (
-                <ContentItem
-                    title={title}
-                    content={content}
-                    key={key}
-                />
-            )
-        })
+    deletePost = (ID) => {
+        const { loginData } = this.props;
+        const data = { ...loginData, ID };
+        console.log(data)
+        this.db.deletePost(data)
+            .then(res => console.log(res))
     }
 
     get createRoute() {
         const { menuList } = this.props;
 
         if (!menuList.length) return false;
-
-        return menuList.map(({ link, postList }, key) => {
+        // console.log(menuList)
+        return menuList.map(({ link, postList, ID }, key) => {
             return (
                 <Route
                     path={link}
-                    render={() => this.generatePosts(postList)}
+                    render={() => this.generatePosts(postList, ID)}
                     key={key}
                 />
             );
         });
     }
+
+    generatePosts = (menuItem, idMenu) => {
+        const { statusAuthoriz, statusEdit } = this.props;
+
+        const content = menuItem.map((post, key) => {
+            // console.log(post)
+            // const { title, content, ID } = post
+            return (
+                <ContentItem
+                    {...post}
+                    {...statusAuthoriz}
+                    key={key}
+                    deletePost={(idPost) => this.deletePost(idPost)}
+                />
+            )
+        })
+
+
+        const btnText = statusEdit ? 'Назад' : 'Добавить пост';
+
+        return (
+            <>
+                {statusAuthoriz && <button onClick={() => this.clickAddPost(idMenu)}>{btnText}</button>}
+
+                {statusEdit ? <FormAddPost menuItem={menuItem} ID_MENU={idMenu} /> : content}
+            </>
+        );
+
+    }
+
+    clickAddPost = (idMenu) => {
+        const { toggleEditContent } = this.props;
+        toggleEditContent();
+    }
+
 
     render() {
         return (
@@ -50,12 +88,27 @@ class Content extends Component {
     }
 }
 
+
+
+
+const mapStateToProps = state => {
+    // console.log(selectors.toggleEditContent(state));
+
+    return {
+        menuList: selectors.menuList(state),
+        statusAuthoriz: selectors.statusAuthoriz(state),
+        statusEdit: selectors.toggleEditContent(state),
+        loginData: selectors.loginData(state),
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    const { toggleEditContent } = bindActionCreators(actions, dispatch);
+    return { toggleEditContent };
+}
+
 const Te = () => {
     return <div>Hello!</div>;
 }
 
-const mapStateToProps = state => {
-    return { menuList: selectors.menuList(state) }
-}
-
-export default connect(mapStateToProps)(Content);
+export default connect(mapStateToProps, mapDispatchToProps)(Content);
