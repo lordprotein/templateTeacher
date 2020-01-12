83b6678e88a53ceff9bccdc3d58b5e93bb3943ed
lordprotein <core.safecrlf>
2020-01-12 23:15:51 +0500

    change a structure props

--- app/src/component/contentItem/ContentItem.js
+++ app/src/component/contentItem/ContentItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const ContentItem = ({ ID, title, content, login, deletePost }) => {
+const ContentItem = ({ postData, statusAuthoriz, deletePost, ID_MENU }) => {
+    const { ID, title, content } = postData;
+    console.log(`ID menu ${ID_MENU}`)
     return (
+        
         <>
             <h2 className="conten__title">{title}</h2>
             <p className="content__text">
                 {content}
             </p>
-            {login && (
+            {statusAuthoriz && (
                 <>
-                    <button>Редактироватьй</button>
-                    <button onClick={() => deletePost(ID)}>Удалить</button>
+                    <button>Редактировать</button>
+                    <button onClick={() => deletePost(ID, ID_MENU)}>Удалить</button>
                 </>
             )}
         </>
