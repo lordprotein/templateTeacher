9f6d67bd57976a367b72f62a33f5de8c66b14af6
lordprotein <core.safecrlf>
2020-01-12 16:14:11 +0500

    add action toggleEditContent

--- app/src/redux/actions.js
+++ app/src/redux/actions.js
@@ -1,2 +1,4 @@
 export const setFetchMenu = value => ({ type: 'SET_FETCH_MENU', value });
 export const setLogIn = value => ({ type: 'SET_LOGIN', value });
+export const toggleEditContent = () => ({ type: 'TOGGLE_EDIT_CONTENT' });
+// export const addPost = value=> ({ type: 'ADD_POST', value });
