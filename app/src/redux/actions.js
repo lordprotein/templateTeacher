6ad25f97eabe5b2ac6fabb3a2c20dc7ed0eaf462
lordprotein <core.safecrlf>
2020-01-15 18:11:18 +0500

    add action for REMOVE_ALL_MODES

--- app/src/redux/actions.js
+++ app/src/redux/actions.js
@@ -7,3 +7,4 @@ export const a_updateContent = value => ({ type: 'UPDATE_POSTS', value });
 export const a_toToggleAddMenu = value => ({ type: 'TOGGLE_ADD_MENU', value });
 export const a_toToggleAddPost = value => ({ type: 'TOGGLE_ADD_POST', value });
 export const a_toToggleEditPost = value => ({ type: 'TOGGLE_EDIT_POST', value });
+export const a_removeAllModes = () => ({ type: 'REMOVE_ALL_MODES' });
