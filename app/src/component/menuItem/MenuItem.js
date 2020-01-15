86c91467c7c9c60df57970e72873b44a79f66cc5
lordprotein <core.safecrlf>
2020-01-15 21:45:19 +0500

    formatting code

--- app/src/component/menuItem/MenuItem.js
+++ app/src/component/menuItem/MenuItem.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom';
 
 const MenuItem = ({ title, link, removeAllModes }) => {
 
-
     return (
-        <Link to={link} className="menu__link" onClick={removeAllModes}>
+        <Link
+            to={link}
+            className="menu__link"
+            onClick={removeAllModes}
+        >
             {title}
         </Link>
     );
