3f84c540acd913c179e54ba5312290ceda9e729b
lordprotein <core.safecrlf>
2020-01-15 18:16:33 +0500

    add removeAllModels to menuItem

--- app/src/component/menuItem/MenuItem.js
+++ app/src/component/menuItem/MenuItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const MenuItem = ({ title, onClickElem, link }) => {
+const MenuItem = ({ title, link, removeAllModes }) => {
 
 
     return (
-        <Link to={link} className="menu__link" onClick={onClickElem}>
+        <Link to={link} className="menu__link" onClick={removeAllModes}>
             {title}
         </Link>
     );
