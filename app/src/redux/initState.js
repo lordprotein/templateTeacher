9c8c5cd5aac76d61213c4b6ff7478c720d502573
lordprotein <core.safecrlf>
2020-01-16 20:28:52 +0500

    add default state to initState

--- app/src/redux/initState.js
+++ app/src/redux/initState.js
@@ -14,6 +14,7 @@ const initState = {
         },
         Menu: {
             modeAddMenu: false,
+            modeEditMenu: false,
             // title: '',
             // content: '',
         }
