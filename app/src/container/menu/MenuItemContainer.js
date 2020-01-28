64398abaf1a438790774c9922bb8c5d720842f68
lordprotein <core.safecrlf>
2020-01-28 15:28:05 +0500

    add localState to MenuItemContainer

--- app/src/container/menu/MenuItemContainer.js
+++ app/src/container/menu/MenuItemContainer.js
@@ -11,7 +11,12 @@ class MenuItemContainer extends Component {
     constructor(props) {
         super(props);
         this.db = new dbService();
+        this.state = {
+            isAddSubmenu: false,
+            isEdit: false,
+        }
     }
+
     disableDomActions = e => {
         e.preventDefault();
         e.stopPropagation();
@@ -23,7 +28,7 @@ class MenuItemContainer extends Component {
         const { db } = this;
 
         const {
-            a_removeAllModes, a_updateMenu,
+            a_updateMenu,
             loginData,
             menuItem: { ID, position } } = this.props;
 
@@ -37,7 +42,6 @@ class MenuItemContainer extends Component {
             .then(() => {
                 db.updateMenuList(position, menuList => {
                     a_updateMenu(menuList);
-                    a_removeAllModes();
                 });
             });
     }
@@ -48,7 +52,7 @@ class MenuItemContainer extends Component {
         const { db } = this;
 
         const {
-            a_removeAllModes, a_updateMenu,
+            a_updateMenu,
             loginData,
             menuItem: { title, ID, position } } = this.props;
 
@@ -65,96 +69,82 @@ class MenuItemContainer extends Component {
             .then(() => {
                 db.updateMenuList(position, menuList => {
                     a_updateMenu(menuList);
-                    a_removeAllModes();
+                    this.onEdit(false);
                 });
             });
     }
 
-    onEdit = e => {
-        this.disableDomActions(e);
+    handleAddSubmenu = () => {
+        const { addSubmenuItem, menuItem: { ID } } = this.props;
 
-        const { a_toToggleEditMenu, menuItem: { ID } } = this.props;
-        this.current_item_ID = ID;
-
-        a_toToggleEditMenu(true);
+        addSubmenuItem(this.input_text, ID);
+        this.onAddSubmenu(false);
     }
 
-    onAddSubmenu = e => {
-        this.disableDomActions(e);
-
-        const { a_toToggleAddSubMenu, menuItem: { ID } } = this.props;
-        this.current_item_ID = ID;
-
-        a_toToggleAddSubMenu(true);
+    onToggleShow = (variable, isToggle, e) => {
+        if (e) this.disableDomActions(e);
+        this.setState({ [variable]: isToggle });
     }
 
-    handleSaveInput = e => {
+    onEdit = (isToggle, e = false) => this.onToggleShow('isEdit', isToggle, e);
+
+    onAddSubmenu = (isToggle, e = false) => this.onToggleShow('isAddSubmenu', isToggle, e);
+
+    onChangeInput = e => {
         return this.input_text = e.target.value;
     }
 
     getEditPanel = () => {
-        const {
-            a_removeAllModes,
-            menuItem: { title }
-        } = this.props;
-
+        const { menuItem: { title } } = this.props;
+        
         return (
             <>
-                <input type="text" defaultValue={title} onChange={this.handleSaveInput} />
-                <button onClick={this.handleEdit}>OK</button>
-                <button onClick={a_removeAllModes}>Отмена</button>
+                <input type="text" defaultValue={title} onChange={this.onChangeInput} />
+                <button onClick={this.handleEdit}>
+                    OK
+                </button>
+                <button onClick={() => this.onEdit(false)}>
+                    Отмена
+                </button>
             </>
         );
     }
 
-    onAddFormAddingSubmenu = () => {
-        const { current_item_ID } = this;
+    onAddFormAddingSubmenu = () => { //For add submenu
+        const { isAddSubmenu } = this.state;
 
-        const {
-            statusAddSubMenu,
-            menuItem: { ID }
-        } = this.props;
+        if (!isAddSubmenu) return false;
 
-        if (statusAddSubMenu && current_item_ID === ID) { //"current_item_ID" - render form for a particular element
-            delete this.current_item_ID; //clear to cansel saving current_item_ID
-            return (
-                <>
-                    <input type="text" onChange={this.handleSaveInput} />
-                    <button onClick={() => this.props.addSubmenuItem(this.input_text, ID)}>
-                        Добавить
-                    </button>
-                </>
-            );
-        }
+        return (
+            <>
+                <input type="text" onChange={this.onChangeInput} />
+                <button onClick={() => this.handleAddSubmenu()}>
+                    Добавить
+                </button>
+            </>
+        );
+
 
     }
 
 
     render() {
-        const { current_item_ID } = this;
-        const {
-            removeAllModes,
-            statusEdit,
-            menuItem, menuItem: { ID } } = this.props;
+        const { isEdit } = this.state;
+        const { menuItem } = this.props;
 
-
-        if (statusEdit && current_item_ID === ID) {
-            delete this.current_item_ID;
-            return this.getEditPanel();
-        }
-
-        const actions = {
-            onEdit: (e) => this.onEdit(e),
-            handleDelete: (e) => this.handleDelete(e),
-            onAddSubmenu: (e) => this.onAddSubmenu(e),
-        }
+        if (isEdit) return this.getEditPanel();
 
         return (
             <MenuItem
                 menuItem={menuItem}
-                actions={actions}
+                actions={
+                    {
+                        onEdit: e => this.onEdit(true, e),
+                        handleDelete: e => this.handleDelete(e),
+                        onAddSubmenu: e => this.onAddSubmenu(true, e),
+                    }
+                }
                 childrens={this.props.children}
-                removeAllModes={removeAllModes}
                 formAddSubmenu={() => this.onAddFormAddingSubmenu()}
             />
         );
@@ -163,23 +153,15 @@ class MenuItemContainer extends Component {
 
 
 
-
-
-
 const mapStateToProps = state => {
     return {
-        statusEdit: selectors.s_toggleEditMenu(state),
-        statusAddSubMenu: selectors.s_toggleAddSubMenu(state),
         loginData: selectors.loginData(state),
     }
 }
 
 const mapDispatchToProps = dispatch => {
-    const { a_toToggleEditMenu, a_toToggleAddSubMenu, a_removeAllModes, a_updateMenu } = bindActionCreators(actions, dispatch);
+    const { a_updateMenu } = bindActionCreators(actions, dispatch);
     return {
-        a_toToggleEditMenu,
-        a_toToggleAddSubMenu,
-        a_removeAllModes,
         a_updateMenu
     }
 }
