bbc53b5f7b4a3a9730a2435aecff858282358f70
lordprotein <core.safecrlf>
2020-01-28 13:06:03 +0500

    fix name folder App

--- app/src/index.js
+++ app/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './container/app/App';
+import App from './container/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import reducer from './redux/reducer';
 
 
 const AppRender = () => {
     const store = createStore(reducer);
-    
+    console.log(3)
     return (
         <Provider store={store}>
             <BrowserRouter>
