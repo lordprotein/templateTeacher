95ebe033bf92d8c3e971ced5425b997891d2b8aa
lordprotein <core.safecrlf>
2020-01-30 13:35:44 +0500

    move store to a separate file

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './container/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import reducer from './redux/reducer';
+import { store } from './redux/store';
 
 
 const AppRender = () => {
-    const store = createStore(reducer);
 
     return (
         <Provider store={store}>
