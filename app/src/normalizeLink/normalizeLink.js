a3227cad09dbffae08416b34b081f524b0b90a75
lordprotein <core.safecrlf>
2020-02-11 22:32:28 +0500

    delete normalizeLink

--- app/src/normalizeLink/normalizeLink.js
+++
@@ -1,9 +0,0 @@
-const toNormalizeLink = (link) => {
-    link = link.toLowerCase();
-    
-    if (!link.includes(' ')) return link;
-    
-    return link.replace(/\s+/g, '_');
-}
-
-export default toNormalizeLink;
