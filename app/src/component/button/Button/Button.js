1ac3b7dadb919ba08b57eab3177c4482de1211d7
lordprotein <core.safecrlf>
2020-02-19 00:05:37 +0500

    add new prop for button

--- app/src/component/button/Button/Button.js
+++ app/src/component/button/Button/Button.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { withLogIn } from '../../../Hoc/withLogIn/withLogIn';
+import styles from './Button.module.css';
 
+export const Button = ({ title, onClick, className, hint }) => {
+    const btnStyles = styles.btn + ' ' + className;
 
-export const Button = ({ title, onClick, className }) => {
     return (
-        <button onClick={onClick} className={className}>
+        <button onClick={onClick} className={btnStyles} title={hint} >
             {title}
         </button>
     );
