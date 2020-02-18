25e4d84fc10aafc4006a9282669fe49f2b6de5f4
lordprotein <core.safecrlf>
2020-02-18 17:26:09 +0500

    add styles and if for childrens

--- app/src/component/menu/MenuItem/MenuItem.js
+++ app/src/component/menu/MenuItem/MenuItem.js
@@ -1,25 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './MenuItem.module.css';
+
 
 export const MenuItem = ({ menuItemData, childrens, getAddSubmenuPanel, getButtonsPanel }) => {
     const { title, link } = menuItemData;
+    const formAddPanel = getAddSubmenuPanel();
+    // console.log(childrens)
     // console.log(link)
     return (
-        <div className="menu__item">
+        <div className={styles.menu__item}>
             <Link
                 to={link}
-                className="menu__link"
+                className={styles.menu__link}
             >
                 {title}
                 {getButtonsPanel()}
             </Link>
+            {
+                childrens
+                    ? (
+                        <div className={styles.menu__sub}>
+                            {childrens}
+                            {formAddPanel && (
+                                <div className={styles.menu__item}>
+                                    {formAddPanel}
+                                </div>
+                            )}
+                        </div>
+                    )
+                    :
+                    (!childrens && formAddPanel)
+                        ? (
+                            <div className={styles.menu__sub}>
+                                <div className={styles.menu__item}>
+                                    {formAddPanel}
+                                </div>
+                            </div>
+                        )
+                        :
+                        false
+            }
+            {/* {
+                formAddPanel && !childrens
+                    ? (
+                        <div className={styles.menu__sub}>
+                            <div className={styles.menu__item}>
+                                {formAddPanel}
+                            </div>
+                        </div>
+                    )
+                    : false
+            } */}
 
-            <div className="menu__sub">
-                {childrens}
-                <div className="menu__item">
-                    {getAddSubmenuPanel()}
-                </div>
-            </div>
         </div >
     );
 }
