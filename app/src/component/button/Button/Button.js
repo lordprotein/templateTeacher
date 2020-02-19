aa37524329d6b3139f5c03bd53da6ef950780c80
lordprotein <core.safecrlf>
2020-02-19 21:19:56 +0500

    add styles for buttons

--- app/src/component/button/Button/Button.js
+++ app/src/component/button/Button/Button.js
@@ -3,7 +3,8 @@ import { withLogIn } from '../../../Hoc/withLogIn/withLogIn';
 import styles from './Button.module.css';
 
 export const Button = ({ title, onClick, className, hint }) => {
-    const btnStyles = styles.btn + ' ' + className;
+    // const btnStyles = styles.btn + ' ' + className;
+    const btnStyles = className ? className : styles.btn;
 
     return (
         <button onClick={onClick} className={btnStyles} title={hint} >
