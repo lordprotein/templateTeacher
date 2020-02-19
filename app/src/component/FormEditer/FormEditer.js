919806dca478c70a8fb5d6ec88d8dc0064758a79
lordprotein <core.safecrlf>
2020-02-19 22:58:19 +0500

    add styles for form

--- app/src/component/FormEditer/FormEditer.js
+++ app/src/component/FormEditer/FormEditer.js
@@ -1,36 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './FormEditer.module.css';
 
 
 export const FormEditer = ({ title, content, actions, toReset }) => {
 	const { onChangeTitleInput, onChangeContentInput, handleSend } = actions;
 
 	return (
-		<div className="editer">
-			<input // title 
-				type="text"
-				placeholder="title"
-				onChange={onChangeTitleInput}
-				defaultValue={title}
-			/>
+		<div className={styles.editWrap}>
+			<div className={styles.section}>
+				<h2 className={styles.section_title}>Заголовок</h2>
+				<input // title 
+					type="text"
+					placeholder="Введите заголовок"
+					onChange={onChangeTitleInput}
+					defaultValue={title}
+					className={styles.titleInput}
+					autoFocus={true}
+					tabIndex="1"
 
-			<textarea //content
-				placeholder="Type content"
-				onChange={onChangeContentInput}
-				defaultValue={content}
-			>
-			</textarea>
+				/>
+			</div>
+			<div className={styles.section}>
+				<h2 className={styles.section_title}>Содержание</h2>
 
-			<input //btn SEND
-				type="submit"
-				value="Ок"
-				onClick={handleSend}
-			/>
+				<div className={styles.panel}>
+					<button className={styles.panel_bold} />
+					<button className={styles.panel_italic} />
+					<button className={styles.panel_link} />
+					<button className={styles.panel_img} />
+					<button className={styles.panel_file} />
+				</div>
 
-			<button onClick={toReset}>
-				Отмена
-            </button>
-		</div>
+				<textarea //content
+					placeholder="Введите текст"
+					onChange={onChangeContentInput}
+					defaultValue={content}
+					className={styles.contentInput}
+					tabIndex="2"
+
+				>
+				</textarea>
+			</div>
+			<div className={styles.btnWrap}>
+				<button onClick={toReset} className={styles.btnFalse}>
+					Отмена
+            	</button>
+
+				<button onClick={handleSend} className={styles.btnTrue}>
+					Сохранить
+				</button>
+
+			</div>
+		</div >
 	);
 }
 
