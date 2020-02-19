9c821dbddb9f74f12f8695c455a2261faaafc4d9
lordprotein <core.safecrlf>
2020-02-19 21:34:31 +0500

    change check

--- app/src/container/page/PageContainer.js
+++ app/src/container/page/PageContainer.js
@@ -11,75 +11,80 @@ import * as actions from '../../redux/actions';
 
 
 class PageContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.db = new dbService();
-        this.state = {
-            isEdit: false,
-            postList: [],
-        }
-    }
+   constructor(props) {
+      super(props);
+      this.db = new dbService();
+      this.state = {
+         isEdit: false,
+         postList: [],
+      }
+   }
 
-    componentDidMount = () => {
-        const { ID_MENU, a_updateContent } = this.props;
-        this.db.getContentList(ID_MENU)
-            .then(res => a_updateContent(res));
-    }
+   componentDidMount = () => {
+      const { ID_MENU, a_updateContent } = this.props;
+      this.db.getContentList(ID_MENU)
+         .then(res => a_updateContent(res));
+   }
 
-    setModeAddPost = isToggle => this.setState({ isEdit: isToggle });
+   setModeAddPost = isToggle => this.setState({ isEdit: isToggle });
 
-    getFormEditer = () => {
-        const { ID_MENU } = this.props;
-        return (
-            <FormEditerContainer
-                ID_MENU={ID_MENU}
-                action="add"
-                toReset={() => this.setModeAddPost(false)}
+   getFormEditer = () => {
+      const { ID_MENU } = this.props;
+      return (
+         <FormEditerContainer
+            ID_MENU={ID_MENU}
+            action="add"
+            toReset={() => this.setModeAddPost(false)}
+         />
+      );
+   }
+
+
+   getPostList = () => {
+      const { postList } = this.props;
+
+      return postList.map((postData) => {
+         return (
+            <PageItemContainer
+               postData={postData}
+               key={postData.ID}
             />
-        );
-    }
+         );
+      })
+   }
 
+   render() {
+      const { isEdit } = this.state;
 
-    getPostList = () => {
-        const { postList } = this.props;
-
-        return postList.map((postData) => {
-            return (
-                <PageItemContainer
-                    postData={postData}
-                    key={postData.ID}
-                />
-            );
-        })
-    }
-
-    render() {
-        const { isEdit } = this.state;
-
-        return (
-            <Page>
-                {isEdit ||
-                    <ButtonWithLogIn
-                        title="Добавить пост"
-                        onClick={() => this.setModeAddPost(true)}
-                    />
-                }
-                {isEdit || this.getPostList()}
-                {isEdit && this.getFormEditer()}
-            </Page>
-        );
-    }
+      return (
+         <Page>
+            {
+               isEdit
+                  ? this.getFormEditer()
+                  : (
+                     <>
+                        <ButtonWithLogIn
+                           title="Добавить пост"
+                           onClick={() => this.setModeAddPost(true)}
+                        />
+                        {this.getPostList()}
+                     </>
+                  )
+            }
+         </Page>
+      );
+   }
 }
 
 const mapStateToProps = state => {
-    return {
-        postList: selectors.getPostList(state),
-    }
+   return {
+      postList: selectors.getPostList(state),
+   }
 }
 
 const mapDispatchToProps = dispatch => {
-    const { a_updateContent } = bindActionCreators(actions, dispatch);
-    return { a_updateContent };
+   const { a_updateContent } = bindActionCreators(actions, dispatch);
+   return { a_updateContent };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PageContainer);
