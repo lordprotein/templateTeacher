69a898694f48334eb5fa7ec66e187f7e82258cea
lordprotein <core.safecrlf>
2020-02-25 20:51:05 +0500

    add show all files

--- app/src/container/formDownloadFile/FileListContainer.js
+++ app/src/container/formDownloadFile/FileListContainer.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { FileList } from '../../component/formDownloadFile/FileList/FileList';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../../redux/actions';
 import { selectors } from '../../redux/reducer';
+import dbService from '../../service/service';
+import FileItemContainer from './FileItemContainer';
 
 
 
 class FileListContainer extends Component {
+    state = {
+        items: [],
+        typeFiles: '',
+    }
+
+    componentDidMount = () => {
+        const { getTypeFiles, postID } = this.props;
+        const db = new dbService();
+
+        console.log(postID, getTypeFiles)
+        db.getFiles(postID, getTypeFiles)
+            .then(itemList => {
+                console.log(itemList);
+                this.setState({ items: itemList, typeFiles: itemList[0].type });
+            })
+    }
+
     toBack = () => {
         const { a_toggleDownloadFilesForm } = this.props;
         a_toggleDownloadFilesForm(false);
     }
 
+    getItemList = () => {
+        const { items } = this.state;
+
+        return items.map(itemData => {
+            return (
+                <FileItemContainer
+                    fileData={itemData}
+                    key={itemData.ID}
+                />
+            );
+        })
+    }
+
     render() {
-        const { getTypeFiles } = this.props;
-        console.log(getTypeFiles)
+        const { typeFiles } = this.state;
 
         return (
             <FileList
-                title={getTypeFiles}
+                title={typeFiles}
                 toBack={() => this.toBack()}
             >
+                {this.getItemList()}
             </FileList>
         );
     }
@@ -30,7 +62,7 @@ class FileListContainer extends Component {
 
 const mapStateToProps = state => {
     return {
-        getTypeFiles: selectors.getTypeFiles(state),
+        getTypeFiles: selectors.getTypeFiles(state), // To delete
     }
 }
 
