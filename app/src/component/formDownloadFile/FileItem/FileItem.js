d713bc94365e2b72a9b727014f2970f0918e878d
lordprotein <core.safecrlf>
2020-02-25 21:19:19 +0500

    add propTypes ito FileItem

--- app/src/component/formDownloadFile/FileItem/FileItem.js
+++ app/src/component/formDownloadFile/FileItem/FileItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import styles from './FilteItem.module.css';
+import PropTypes from 'prop-types';
 
-export const FileItem = ({ fileType, path }) => {
+
+export const FileItem = ({ type, path }) => {
     let link = `http://localhost:3333`;
 
-    switch (fileType) {
+    switch (type) {
         case 'image': {
             return (<img src={`${link}/${path}`} className={styles.item} alt="img" />);
         }
         default: return (<div className={styles.item}></div>);
     }
-} 
+}
+
+FileItem.propTypes = {
+    type: PropTypes.string.isRequired,
+    path: PropTypes.string
+}
