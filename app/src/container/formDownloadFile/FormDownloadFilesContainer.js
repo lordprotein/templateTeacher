3e046e6a0ab76c90a3c0c1dc4e161f1ab65dd29c
lordprotein <core.safecrlf>
2020-02-26 20:22:42 +0500

    add methods to FormDownloadFilesContainer

--- app/src/container/formDownloadFile/FormDownloadFilesContainer.js
+++ app/src/container/formDownloadFile/FormDownloadFilesContainer.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react'
 import { FormDownloadFiles } from '../../component/formDownloadFile/FormDownloadFiles/FormDownloadFiles';
+import dbService from '../../service/service';
 
 class FormDownloadFilesContainer extends Component {
     constructor(props) {
         super(props);
+        this.action = null;
         this.titleEnd = this.getSwitchForm();
     }
 
     getSwitchForm = () => {
         const { downloadFrom } = this.props;
 
-        if (downloadFrom === 'local') return "компьютера";
-        if (downloadFrom === 'url') return "интернета";
+        if (downloadFrom === 'local') {
+            this.action = this.handleSubmitForLocal;
+            return "компьютера";
+        };
+        if (downloadFrom === 'url') {
+            this.action = this.handleSubmitForUrl;
+            return "интернета";
+        }
+    }
+
+    handleInputValue = e => {
+        const value = e.target;
+        this.value = value;
+        // console.log(e.target)
+    }
+
+
+    handleSubmitForLocal = () => {
+        let { value } = this;
+        const { postID } = this.props;
+
+        if (!value) return console.log('Haven`t a value');
+        value = value.files[0];
+
+        const db = new dbService();
+        
+        db.downloadImg(postID, value)
+            .then(res => console.log(res))
+    }
+
+    handleSubmitForUrl = () => {
+        console.log('url func')
     }
 
     render() {
@@ -21,6 +53,8 @@ class FormDownloadFilesContainer extends Component {
         return <FormDownloadFiles
             titleEnd={titleEnd}
             downloadFrom={downloadFrom}
+            handleInputValue={e => this.handleInputValue(e)}
+            onSubmit={() => this.action()}
         />
     }
 }
