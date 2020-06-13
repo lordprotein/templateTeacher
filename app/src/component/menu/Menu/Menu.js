85ae21a95fd2a5e04b60286f420c331661abb7f4
lordprotein <gamee.logi@gmail.com>
2020-06-13 19:29:26 +0500

    fix

--- app/src/component/menu/Menu/Menu.js
+++ app/src/component/menu/Menu/Menu.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styles from './Menu.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { withLogIn } from '../../../Hoc/withLogIn/withLogIn';
 
 export const Menu = ({ children, stylePos }) => {
     let menuStyles = styles.menu;
     let titleStyles;
+
+    const LinkToSettings = () => { return <Link to={'/settings'}>Настройки</Link> };
+    const Settings = withLogIn(LinkToSettings);
+
     if (stylePos === 'top') {
         menuStyles = `${menuStyles} ${styles.menuInline}`;
     }
+
     if (stylePos === 'left') {
         titleStyles = (
             <>
-                <Link to={'/settings'}>Настройки</Link>
+                <Settings />
                 <h2 className={styles.title}>Главное меню</h2>
             </>
         );
